d0d38648813eec4d1ea619ffb5f540b0cfa96ed7
Iván Corrales <developer@wesovi.com>
2016-03-03 06:21:35 +0100

    feat: adding more changes to the project

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,19 +1,16 @@
-var supertest = require("supertest");
-var should = require("should");
+const supertest = require('supertest');
+const should = require('should');
+const app = require('../examples/rest-api/server');
 
-var app = require('../examples/rest-api/server');
-
-describe('Testing with the response is empty', function() {
-
-    it('should  list shop details', function(done) {
-
-        supertest(app)
-            .get('/api/v1/shops/1')
-            .expect("Content-type",/json/)
-            .end(function(err,res){
-                res.status.should.equal(200);
-                res.type.should.equal('application/hal+json');
-                done();
-            });
-    });
+describe('Testing with the response is empty', function () {
+  it('should  list shop details', function (done) {
+    supertest(app)
+      .get('/api/v1/shops/1')
+      .expect('Content-type', /json/)
+      .end(function (err, res) {
+        res.status.should.equal(200);
+        res.type.should.equal('application/hal+json');
+        done();
+      });
+  });
 });
