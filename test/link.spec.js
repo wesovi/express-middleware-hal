0386863a38de9adbb45b6be8ba20b13ebae28c34
Iván Corrales <developer@wesovi.com>
2016-03-03 06:38:54 +0100

    feat: adding more changes

---
+++ test/link.spec.js
@@ -0,0 +1,9 @@
+const should = require('should');
+const Link = require('./../lib/link').Link;
+
+describe('Testing Link function', function () {
+  it('should  creating the Link object correctly', function () {
+    const link = new Link();
+
+  });
+});
